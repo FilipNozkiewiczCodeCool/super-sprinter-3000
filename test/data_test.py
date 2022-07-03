7649430d75aaae61d7f3ab4271bb26d91bc89f82
Pastor28 <dgorz@interia.com>
2022-07-03 11:51:26 +0200

    Added server modification

--- test/data_test.py
+++ test/data_test.py
@@ -1,9 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
-#from test.data_test import *
-import data_handler
-
-app = Flask(__name__)
-
+x = 100
 list_of_dict = [{"id": 12,
                  "story_title": "wyslij story title",
                  "user_story": "wyslij user story",
@@ -20,17 +15,3 @@ list_of_dict = [{"id": 12,
                  "estimation": "wyslij estimation",
                  "status": "wyslij status"
                  }]
-
-@app.route('/')
-@app.route('/list')
-def route_list():
-    user_stories = list_of_dict #data_handler.get_all_user_story()
-
-    return render_template('list.html', user_stories=user_stories)
-
-if __name__ == '__main__':
-    app.run(
-        host='0.0.0.0',
-        port=8000,
-        debug=True,
-    )
